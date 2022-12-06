783010aa0a9bca78b341b23e22202dce2c70fcb2
tuanlinhtony <mrpi3990@gmail.com>
2022-12-06 10:08:31 +0700

    Finish flow auto test for create new device category NMS

--- src/routers/pageRouter.js
+++ src/routers/pageRouter.js
@@ -47,6 +47,9 @@ router.post('/create-test-case', async(req, res) => {
 
 router.get('/loginTesting',  async(req,res) => {
 
+    // const tenDanhMucThietBi = "Linh test lần thứ " + uuidv4();
+    // generating random integers within a range
+    const tenDanhMucThietBi = "Danh mục thiết bị số " + Math.floor(Math.random() * 9999)
     let driver = await new Builder().forBrowser(Browser.CHROME).build();
     try {
         //Đăng nhập vào web
@@ -64,10 +67,38 @@ router.get('/loginTesting',  async(req,res) => {
         await driver.findElement(By.xpath('//a[@class="menu-item ng-tns-c9-18 ng-star-inserted"]')).click();
         await driver.sleep(time_delay)
 
-        //Click menu Khai báo danh mục
-        await driver.findElement(By.xpath('//li[1]/a')).click();
+        //Click menu Danh mục thiết bị
+        await driver.findElement(By.xpath('//li[2]/a')).click();
 
+        //Thêm danh mục thiết bị
+        await driver.findElement(By.xpath('//div[@class="btn-import"]/button')).click();
 
+        //Điền Tên danh mục thiết bị
+
+        await driver.findElement(By.xpath('//div[@class="p-lg-9"]/input')).sendKeys(tenDanhMucThietBi);
+        Testcase.update({testcaseID: "DAS_32"}, {
+            expectedResult: tenDanhMucThietBi
+        }, function(err, affected, resp) {
+            console.log(resp);
+        })
+        //Lựa chọn Nhà sản xuất:
+        await driver.sleep(time_delay)
+        await driver.findElement(By.xpath('//div[2]/div/div[2]/ng-select/div/div/div[2]/input')).click()
+        await driver.sleep(time_delay)
+        await driver.findElement(By.xpath('//div[3]/span')).click()
+
+        //Lựa chọn Trạng thái hoạt động
+        await driver.sleep(time_delay)
+        await driver.findElement(By.xpath('//div[5]/div/div[2]/ng-select/div/div/div[3]/input')).click()
+        await driver.sleep(time_delay)
+        await driver.findElement(By.xpath('//span[contains(.,"Hoạt động")]')).click()
+
+        //Điền Lớp thiết bị
+        await driver.findElement(By.xpath("//div[6]/div/div[2]/input")).sendKeys('testtt');
+
+        //Bấm ghi lại
+        await driver.sleep(time_delay)
+        await driver.findElement(By.xpath('//button[contains(.,"Ghi lại")]')).click()
         //bấm button đăng xuất
         // await driver.findElement(By.xpath("//li[contains(@class, 'p-d-flex log-out')]")).click();
         // let login_container = await driver.findElement(By.xpath("//div[contains(@class, 'login-container')]"))
@@ -76,24 +107,44 @@ router.get('/loginTesting',  async(req,res) => {
         // console.log(login_label)
     } finally {
         //await driver.quit();
-        const query = "select count(dc.uid) from device_category dc " +
-            "where name like '%Automation Test lần%' and is_active = 1 and is_deleted = 0";
+        const query = "select dc.name from device_category dc " +
+            "where name like '%" + tenDanhMucThietBi + "%' and is_active = 1 and is_deleted = 0";
 
         pool.connect()
             .then((client) => {
                 client.query(query)
-                    .then(resp => {
+                    .then(async(resp) => {
                         for (var row of resp.rows) {
-                            const result = JSON.stringify(row.count)
+                            const result = JSON.stringify(row.name)
                             myResult = result.replace(/"/g, '');
                             console.log("Ket qua sau khi querry " + myResult);
                             res.send(myResult)
                             Testcase.update({testcaseID: "DAS_32"}, {
-                                receivedResult: myResult
+                                receivedResult: myResult,
+                                status: "2"
                             }, function(err, affected, resp) {
                                 console.log(resp);
                             })
                         }
+
+                        const testcases = await Testcase.find({})
+                        if(testcases[0].expectedResult === testcases[0].receivedResult){
+                            console.log(testcases[0].expectedResult + ' và ' + testcases[0].receivedResult)
+                            Testcase.update({testcaseID: "DAS_32"}, {
+                                testResult: "Đạt"
+                            }, function(err, affected, resp) {
+                                console.log(resp);
+                            })
+                        }else if(testcases[0].expectedResult != testcases[0].receivedResult){
+                            console.log(testcases[0].expectedResult + ' và ' + testcases[0].receivedResult)
+                            Testcase.update({testcaseID: "DAS_32"}, {
+                                testResult: "Không đạt"
+                            }, function(err, affected, resp) {
+                                console.log(resp);
+                            })
+                        }
+
+
                     })
                     .catch(err => {
                         console.error(err);
@@ -103,57 +154,21 @@ router.get('/loginTesting',  async(req,res) => {
                 console.error(err);
             });
     }
-
-    //So sánh kết quả
-    const testcases = await Testcase.find({})
-    console.log(JSON.stringify(testcases.expectedResult));
-    if(testcases.expectedResult === testcases.receivedResult){
-
-
-        Testcase.update({testcaseID: "DAS_32"}, {
-            testResult: "Đạt"
-        }, function(err, affected, resp) {
-            console.log(resp);
-        })
-    }else if(testcases.expectedResult != testcases.receivedResult){
-        Testcase.update({testcaseID: "DAS_32"}, {
-            testResult: "Chưa đạt"
-        }, function(err, affected, resp) {
-            console.log(resp);
-        })
-    }
-
-
 })
 
 router.get('/querryDB',  async(req,res) => {
-    const query = "select count(dc.uid) from device_category dc " +
-        "where name like '%Linh test lan 3%' and is_active = 1 and is_deleted = 0";
+    try{
+        const testcases = await Testcase.find({})
 
-    pool.connect()
-        .then((client) => {
-            client.query(query)
-                .then(resp => {
-                    for (var row of resp.rows) {
-                        const result = JSON.stringify(row.count)
-                        myResult = result.replace(/"/g, '');
-                        console.log("Ket qua sau khi querry " + myResult);
-                        res.send(myResult)
-                        Testcase.update({testcaseID: "DAS_32"}, {
-                            receivedResult: myResult
-                        }, function(err, affected, resp) {
-                            console.log(resp);
-                        })
-                    }
-                })
-                .catch(err => {
-                    console.error(err);
-                });
-        })
-        .catch(err => {
-            console.error(err);
-        });
-
+        console.log(testcases[0].expectedResult)
+        if(testcases[0].expectedResult === testcases[0].receivedResult){
+            console.log("Dat")
+        }else if(testcases[0].expectedResult != testcases[0].receivedResult){
+            console.log("kho dat")
+        }
+    }catch(e){
+        res.status(500).send(e.message)
+    }
 
 })
 
